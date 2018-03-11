fa0739b3c03e26474ac1e651e94c25502d22512e
Kurt Jacobson <kcjengr@gmail.com>
2018-03-11 13:16:21 -0400

    Enable nozzle park and print counter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1169,7 +1169,7 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-//#define NOZZLE_PARK_FEATURE
+#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
@@ -1269,7 +1269,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
